fda8c566b9f9ca596d49917d02fcab4517ff1bc4
canasmh <canasmh@yahoo.com>
2022-05-12 17:55:27 -0400

    render index.ejs when making GET request to home route

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const ejs = require('ejs');
 const PORT = process.env.PORT || 3000
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+const currentYear = new Date().getFullYear();
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
-})
+  res.render('index', {year: currentYear});
+});
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`)
-})
+});
