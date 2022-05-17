02726da9515e9cd5cfac5d54619cc66e0f89ba55
canasmh <canasmh@yahoo.com>
2022-05-17 16:10:50 -0400

    Get rid of commented code an unused components

--- client/src/NavigationMenu.js
+++ client/src/NavigationMenu.js
@@ -1,30 +1,8 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropDown";
 import Container from "react-bootstrap/Container";
 import "./NavigationMenu.css";
-
-    //         <% if (isAuth) { %>
-    //           <li class="nav-item ">
-    //             <a class="nav-link" href="/dashboard">Dashboard</a>
-    //           </li>
-    //           </ul>
-            //   <form action="/logout" method="POST">
-            //     <button class="btn btn-login" type="submit">Log Out</button>
-            //   </form>
-    //         <% } else { %>
-    //           </ul>
-    //           <form >
-    //             <a href="/login"><button class="btn btn-login" type="button">Log In</button></a>
-    //           </form>
-    //           <form >
-    //             <a href="/signup"><button class="btn btn-signup" type="button">Sign Up</button></a>
-    //           </form>
-    //         <% } %>
-    //     </div>
-    //   </div>
-    // </nav>
 
 function NavigationMenu(props) {
     return (
