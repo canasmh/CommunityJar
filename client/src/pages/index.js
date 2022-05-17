a295678d2586c31ee1f387049c10237783e15a2d
canasmh <canasmh@yahoo.com>
2022-05-17 18:55:38 -0400

    Create the home page, this was mostly copy and paste from index.ejs, and adding react-bootstrap components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import LandingPage from "../components/LandingPage";
+import About from "../components/About";
+import ContactForm from "../components/ContactForm";
+
 
 function Home() {
-    return (<h1>This is the home page</h1>)
+    return (
+    <div>
+        <LandingPage />
+        <About />
+        <ContactForm />
+    </div>)
 }
 
 export default Home;
