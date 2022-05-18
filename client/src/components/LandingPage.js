93e581a7e049a4fc983c853d2ad7027ef12ebe1d
canasmh <canasmh@yahoo.com>
2022-05-18 19:53:39 -0400

    Add a use state to track whether user is authorized. Hade log in and sign up buttons if authorized

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -5,6 +5,15 @@ import "./LandingPage.css"
 
 
 function LandingPage() {
+
+    const [isAuth, setIsAuth] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/isAuth")
+        .then(response => response.json())
+        .then(data => setIsAuth(data.isAuth))
+    });
+
     return (
     <div className="landing-page">
         <Row>
@@ -13,8 +22,8 @@ function LandingPage() {
                     <h2 className="landing-header">Challenge Yourself</h2>
                     <hr />
                     <h2 className="landing-header">Plan an Experience</h2>
-                    <a href="/login"><button className="my-btn btn-login2" type="button" name="logIn">Log In</button></a>
-                    <a href="/signup"><button className="my-btn btn-signup2" type="button" name="signUp">Sign Up</button></a>
+                    {!isAuth && <a href="/login"><button className="my-btn btn-login2" type="button" name="logIn">Log In</button></a>}
+                    {!isAuth && <a href="/signup"><button className="my-btn btn-signup2" type="button" name="signUp">Sign Up</button></a>}
                 </div>
             </Col>
             <Col lg="6">
