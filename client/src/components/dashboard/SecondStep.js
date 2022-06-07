9dfdf0a0e77434dc486264c40ec7c99e518cd0a9
canasmh <canasmh@yahoo.com>
2022-06-06 23:43:20 -0400

    Remove the map function. Each column is too different for it to be optimal. Also make sure valid values are entered before continuing to step three

--- client/src/components/dashboard/SecondStep.js
+++ client/src/components/dashboard/SecondStep.js
@@ -7,59 +7,60 @@ import "./AddCommunityJar.css";
 
 function SecondStep(props) {
 
-    function PayOptions(payOption) {
-
-        const evenColumns = (props.id % 2 === 0);
-        const lastColumn = (props.id === payOptions.length);
-
-        return (
-            <Col lg={(props.jarType !== 3) ? "4" : "6"} md={ (!evenColumns && lastColumn) ? "12" : "6"}>
-                
-                {payOption.id === 1 ? <div><h4 className="jar-options-header">{payOption.title}</h4><span>Every</span> 
-                <Form.Select onChange={props.functions[0]} aria-label={payOption.title} value={props.values[0]}>
-                    <option value={null}>0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                </Form.Select> <span>Week(s)</span><p>{payOption.description}</p></div> : null}
-                
-                { (payOption.id === 2) && <div><h4 className="jar-options-header">{payOption.title}</h4><span>$</span>
-                <Form.Control onChange={props.functions[1]} value={props.values[1]} type="text" placeholder="0.00" id="withdrawAmount" name="withdrawAmount" /><p>{payOption.description}</p></div>}
-
-                { (payOption.id ===3 && props.jarType !== 3) ? <div><h4 className="jar-options-header">{payOption.title}</h4><span>Every</span> 
-                <Form.Select onChange={props.functions[2]} aria-label={payOption.title} value={props.values[2]}>
-                    <option value={null}>0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                </Form.Select> <span>Week(s)</span><p>{payOption.description}</p></div> : null}
-                
-            </Col>
-        )
-    };
-
     return (
         <div id="secondStep">
-            <i  class="fa-solid fa-arrow-down"></i>
+            <i  className="fa-solid fa-arrow-down"></i>
             <h2>Step Two</h2>
             <h3>Select Withdraw & Deposity Frequency</h3>
             <Row>
-                {payOptions.map((jar) => (
-                    <PayOptions key={jar.key} title={jar.title} description={jar.description} id={jar.key}/>
-                ))};
+                <Col lg={(props.jarType != 3) ? "4" : "6"} md="6">
+                    <h4 className="jar-options-header">{payOptions[0].title}</h4>
+                    {!props.errors.errorFirst ? null : <p className="error">* Please select a valid value</p>}
+                    <span>Every</span> 
+                    <Form.Select onChange={props.functions[0]} aria-label={payOptions[0].title} value={props.values[0]}>
+                        <option value={null}>0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </Form.Select>
+                    <span>Week(s)</span>
+                    <p>{payOptions[0].description}</p>
+                </Col>
+                <Col lg={(props.jarType != 3) ? "4" : "6"} md="6">
+                    <h4 className="jar-options-header">{payOptions[1].title}</h4>
+                    {!props.errors.errorSecond ? null : <p className="error">* Please select a valid value</p>}
+                    <span>$</span>
+                    <Form.Control onChange={props.functions[1]} value={props.values[1]} type="text" placeholder="0.00" id="withdrawAmount" name="withdrawAmount" />
+                    <p>{payOptions[1].description}</p>
+                </Col>
+                
+                    { (props.jarType !== 3) ? 
+                    <Col lg="4" md="6">
+                        <h4 className="jar-options-header">{payOptions[2].title}</h4>
+                        {!props.errors.errorThird ? null : <p className="error">* Please select a valid value</p>}
+                        <span>Every</span> 
+                        <Form.Select onChange={props.functions[2]} aria-label={payOptions[2].title} value={props.values[2]}>
+                            <option value={null}>0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </Form.Select> 
+                        <span>Week(s)</span>
+                        <p>{payOptions[2].description}</p></Col> : null}
+                
             </Row>
-            <a href="#thirdStep"><button type="button" onClick={props.updateStep}>Continue</button></a>
+            <a href={(!props.errors.errorFirst && !props.errors.errorSecond && !props.errors.errorThird) ? "#thirdStep" : "#secondStep"}><button type="button" onClick={props.updateStep}>Continue</button></a>
         </div>
     )
 }
